be4ab7ada32fb678746f4487c04553265920ddf7
bahtonang <145633527+bahtonang@users.noreply.github.com>
2024-02-15 11:16:04 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -45,3 +45,68 @@ if (isset($_POST['pid']))
 }
 
 ?>
+
+
+Forgive me if this is considered cross posting. I just wanted this to show up for any one searching for file path and renaming. I used various codes on this forum and worked out the following solution.
+Goal : Upload a file (Filename), then set the file name along with it’s path into the table.
+After setting the form, I went to the onApplicationInit event and set the following
+if ((substr($_SERVER[‘HTTP_HOST’],-4) == ‘8090’) OR (substr($_SERVER[‘HTTP_HOST’],-4) == ‘8090’)) { // Current Port?
+$prod = TRUE;
+[fpg] = “…/…/…/file/pdffiles/”;
+
+} else {
+$prod = FALSE;
+[fpg] = “…/_lib/file/doc/pdffiles/”;
+}
+PLEASE NOTE : THE 8090 above could be different in your system
+
+Next against the field general settings (under which the document is uploaded),I set the sub folder value to [fpg]
+
+Next in the after onAfterInsert event I put the following code :
+$fp = [fpg];
+//In the following part I am generating new file name by adding recordID newly generated , I am splitting the filename into 2 parts primary and extension. then I am adding the recordid to the primary part., after that adding the extension
+
+$filename_in_parts = explode(’.’, {document_upload_path});
+$newfilename = $filename_in_parts[0] .$document_id ."." .$filename_in_parts[1]; // existing primary name, record ID, period separator, then extension
+$filename = $fp .{document_upload_path};
+$newfilename = $fp .$newfilename;
+
+//following is actual code to rename the file to above generated new file name
+
+if(file_exists($newfilename))
+    { 
+       echo "Error While Renaming $filename" ;
+    }
+else
+    {
+       if(rename( $filename, $newfilename))
+       { 
+       //echo "Successfully Renamed $filename to $newfilename" ;
+       }
+      else
+      {
+       echo "A File With The Same Name Already Exists" ;
+      }
+    }
+sc_commit_trans(); // THIS IS VERY MUCH NECESSARY. I HAD PROBLEMS without this. I am generating record id using AutoIncrement (Manual).
+// Therefore when I am trying to search for the new record in the update command below the record is not found. I crossed check with the database table and the record was not getting inserted. // So I use the commit trans.
+
+/**
+
+Update a record on another table
+*/
+// SQL statement parameters
+$update_table = ‘documents’; // Table name
+$update_where = “document_id = " .{document_id}; // Where clause
+$update_fields = array( // Field list, add as many as needed
+“document_upload_path = '” .$newfilename .”’",
+
+);
+
+// Update record
+$update_sql = ‘UPDATE ’ . $update_table
+. ’ SET ’ . implode(’, ', $update_fields)
+. ’ WHERE ’ . $update_where;
+echo $update_sql;
+sc_exec_sql($update_sql);
+
